da75b582e753f1cb8a6da69271bbb450e645b675
azdYM <02ym.azad@gmail.com>
2024-12-25 19:26:43 +0300

    TDD first commit

---
+++ src/RomanNumeral.ts
@@ -0,0 +1,101 @@
+
+export class RomanNumeral {
+    of(number: number) {
+        if (this.forRangeOneToThree(number)) 
+            return this.toRomanForRangeOneToThree(number)
+        
+        if (this.forRangeFourToEight(number)) 
+            return this.toRomanForRangeFourToEight(number)
+        
+        if (this.forRangeNineToThirtyNine(number)) 
+            return this.toRomanForRangeNineToThirtyNine(number)
+
+        if (this.forRangeNineToFortyToEightyNine(number)) 
+            return this.toRomanForRangeFortyToEightyNine(number)
+    }
+
+    private forRangeOneToThree(number: number) {
+        return [1, 2, 3].includes(number)
+    }
+
+    private forRangeFourToEight(number: number) {
+        return [4, 5, 6, 7, 8].includes(number)
+    } 
+
+    private forRangeNineToFortyToEightyNine(number: number) {
+        return number >= 40 && number <= 89
+    }
+
+    private forRangeNineToThirtyNine(number: number)
+    {
+        return number >= 9 && number <= 39
+    }
+
+    private toRomanForRangeOneToThree(number: number) {
+        let romanNumeral = ""
+        for(let i = 1; i <= number; i++) {
+            romanNumeral += this.oneToRomanNumeral()
+        }
+        
+        return romanNumeral
+    }
+
+    private toRomanForRangeFourToEight(number: number) {
+        if (number === 5 )
+            return this.fiveToRomanNumeral()
+
+        if (number < 5)
+            return 'IV'
+
+        return this.fiveToRomanNumeral() + this.toRomanForRangeOneToThree(number - 5)
+    }
+
+    private toRomanForRangeNineToThirtyNine(number: number) {
+        if (number === 10) 
+            return this.tenToRomanNumeral()
+        
+        if (number < 10)
+            return 'IX'
+
+        return this.tenToRomanNumeral() + this.toRomanForRangeToThirtyNine(number - 10)
+    }
+
+    private toRomanForRangeFortyToEightyNine(number: number) {
+        if (number === 50)
+            return this.fiftyToRomanNumeral()
+
+        if (number < 50)
+            return this.tenToRomanNumeral() + this.fiftyToRomanNumeral() + this.toRomanForRangeToThirtyNine(40 - number)
+    }
+
+    private toRomanForRangeToThirtyNine(number: number) {
+        if (this.forRangeOneToThree(number)) 
+            return this.toRomanForRangeOneToThree(number)
+
+        if (this.forRangeFourToEight(number))
+            return this.toRomanForRangeFourToEight(number)
+        
+        if (this.forRangeNineToThirtyNine(number)) 
+            return this.toRomanForRangeNineToThirtyNine(number)
+
+        return ''
+    }
+
+    private oneToRomanNumeral() {
+        return 'I'
+    }
+
+    private fiveToRomanNumeral() {
+        return 'V'
+    }
+
+    private tenToRomanNumeral() {
+        return 'X'
+    }
+
+    private fiftyToRomanNumeral() {
+        return 'L'
+    }
+
+   
+}
--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,1 +1,65 @@
 // Write your test here
+
+import { RomanNumeral } from "./RomanNumeral.js"
+
+test('test if 1 print I', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(1)).toBe('I')
+})
+
+test('test if 2 print II', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(2)).toBe('II')
+})
+
+test('test if 4 print IV', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(4)).toBe('IV')
+})
+
+test('test if 5 print V', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(5)).toBe('V')
+})
+
+test('test if 6 print VI', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(6)).toBe('VI')
+})
+
+test('test if 9 print IX', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(9)).toBe('IX')
+})
+
+test('test if 10 print X', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(10)).toBe('X')
+})
+
+test('test if 11 print XI', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(11)).toBe('XI')
+})
+
+test('test if 39 print XXXIX', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(39)).toBe('XXXIX')
+})
+
+test('test if 40 print XL', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(40)).toBe('XL')
+    expect(romanNumeral.of(41)).toBe('XLI')
+
+})
+
+test('test if 50 print L', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(50)).toBe('L')
+})
+
+test('test if 60 print LX', () => {
+    const romanNumeral = new RomanNumeral()
+    expect(romanNumeral.of(60)).toBe('LX')
+})
